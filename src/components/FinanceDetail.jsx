a2a54c0286f9fed05f1ffd4d5c7629d2192b6ae3
Taner <tanerdokmets@gmail.com>
2025-06-12 22:47:52 +0300

    financepage added

---
+++ src/components/FinanceDetail.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import financeImage from "/services/category-4.jpg";
+const FinanceDetail = () => {
+  return (
+    <div>
+      <div className="container mt-5 lg:mt-20 px-2 lg:px-0">
+        <div
+          className="box-border rounded-lg h-auto lg:h-[70vh] 
+                          flex flex-col lg:flex-row transition-all duration-500 
+                          relative overflow-hidden bg-white"
+        >
+          <div className="w-full lg:w-2/3 relative h-[300px] lg:h-full">
+            <img
+              src={financeImage}
+              className="absolute inset-0 w-full h-full object-fill"
+            />
+          </div>
+          <div className="w-full lg:w-1/2 p-6 lg:p-12 flex flex-col justify-center">
+            <div className="max-w-xl">
+              <p className="text-sm lg:text-lg font-medium uppercase">
+                Services we have provided in the Cement Sectors
+              </p>
+              <p className="text-sm lg:text-base mt-3">
+                Turnkey Cement Plants commitments EP +C + F (Financing) We offer
+                a unique EPС+F business model
+              </p>
+              <p className="font-medium text-sm lg:text-lg py-3">FINANCING ALTERNATIVES</p>
+             <ul className="space-y-1">
+              <li>Bank Loan</li>
+              <li>Private Fund Credit</li>
+              <li>Equity Participation</li>
+              <li>EXIM Bank Loan</li>
+             </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FinanceDetail;
--- src/pages/Finance.jsx
+++ src/pages/Finance.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-
+import FinanceDetail from "../components/FinanceDetail";
+import Footer from "../components/Footer"
 const Finance = () => {
   return (
     <div>
       <Navbar />
-      <Hero title="Finance Solutions" image="/finance/finance-hero.jpg"/>
+      <Hero title="Finance Solutions" image="/finance/finance-hero.jpg" />
+      <FinanceDetail />
+      <Footer />
     </div>
   );
 };
